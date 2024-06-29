5a50ab9affcca2dcfb05bf7695808bb09439f948
angelosilveira <angelo.soares.terceiros@claro.com.br>
2024-06-29 00:57:12 -0300

    inital commit

---
+++ src/database/database.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Course } from '../courses/entities/courses.entity'
+import { DataSourceOptions } from 'typeorm'
+import { Tag } from 'src/courses/entities/tags.entity'
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'devtraining',
+  entities: [Course, Tag],
+  synchronize: true,
+}
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        return {
+          ...dataSourceOptions,
+        }
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
