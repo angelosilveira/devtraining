80a3a44455bb1a19948f6ff2f579037c6b5dd2f3
angelosilveira <angelo.soares.terceiros@claro.com.br>
2024-07-01 08:59:50 -0300

    inital commit

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,18 +1,13 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CoursesService } from './courses.service';
+import { CoursesService } from './courses.service'
 
 describe('CoursesService', () => {
-  let service: CoursesService;
+  let service: CoursesService
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CoursesService],
-    }).compile();
-
-    service = module.get<CoursesService>(CoursesService);
-  });
+    service = new CoursesService()
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
