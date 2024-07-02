8c219513110963ed5837162aa441aac559d6325d
angelo <angelo.silveira09@gmail.com>
2024-07-02 11:41:05 -0300

    feat: add dotenvs

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,13 +1,133 @@
+import { randomUUID } from 'node:crypto'
 import { CoursesService } from './courses.service'
 
-describe('CoursesService', () => {
+describe('CoursesService unit tests', () => {
   let service: CoursesService
+  let id: string
+  let createdAt: Date
+  let expectOutPutTags: any
+  let expectOutPutCourses: any
+  let mockCourseRepository: any
+  let mockTagRepository: any
 
   beforeEach(async () => {
     service = new CoursesService()
+    id = randomUUID()
+    createdAt = new Date()
+    expectOutPutTags = [
+      {
+        id,
+        name: 'nestjs',
+        createdAt,
+      },
+    ]
+
+    expectOutPutCourses = [
+      {
+        id,
+        name: 'test',
+        description: 'test description',
+        createdAt,
+        tasg: expectOutPutTags,
+      },
+    ]
+
+    mockCourseRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutPutCourses)),
+      save: jest.fn().mockReturnValue(Promise.resolve(expectOutPutCourses)),
+      update: jest.fn().mockReturnValue(Promise.resolve(expectOutPutCourses)),
+      preload: jest.fn().mockReturnValue(Promise.resolve(expectOutPutCourses)),
+      findAll: jest.fn().mockReturnValue(Promise.resolve(expectOutPutCourses)),
+      find: jest.fn().mockReturnValue(Promise.resolve(expectOutPutCourses)),
+      findOne: jest.fn().mockReturnValue(Promise.resolve(expectOutPutCourses)),
+      remove: jest.fn().mockReturnValue(Promise.resolve(expectOutPutCourses)),
+    }
+
+    mockTagRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutPutTags)),
+      findOne: jest.fn(),
+    }
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('should be create a course', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCourseRepository
+
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository
+
+    const courseCreateDTO = {
+      name: 'test',
+      description: 'test description',
+      tags: ['nestjs'],
+    }
+
+    const newCourse = await service.create(courseCreateDTO)
+    expect(mockCourseRepository.save).toHaveBeenCalled()
+    expect(expectOutPutCourses).toStrictEqual(newCourse)
+  })
+
+  it('should list all courses', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCourseRepository
+
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository
+
+    const courses = await service.findAll()
+
+    expect(mockCourseRepository.find).toHaveBeenCalled()
+    expect(expectOutPutCourses).toStrictEqual(courses)
+  })
+
+  it('should list one course', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCourseRepository
+
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository
+
+    const course = await service.findOne(id)
+
+    expect(mockCourseRepository.findOne).toHaveBeenCalled()
+    expect(expectOutPutCourses).toStrictEqual(course)
+  })
+
+  it('should update a course', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCourseRepository
+
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository
+
+    const updateCourseDTO = {
+      name: 'test',
+      description: 'test description',
+      tags: ['nestjs'],
+    }
+
+    const course = await service.update(id, updateCourseDTO)
+
+    expect(mockCourseRepository.save).toHaveBeenCalled()
+    expect(mockCourseRepository.preload).toHaveBeenCalled()
+    expect(expectOutPutCourses).toStrictEqual(course)
+  })
+
+  it('should remove a course', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCourseRepository
+
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository
+
+    const course = await service.remove(id)
+
+    expect(mockCourseRepository.findOne).toHaveBeenCalled()
+    expect(mockCourseRepository.remove).toHaveBeenCalled()
+    expect(expectOutPutCourses).toStrictEqual(course)
   })
 })
--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,10 +1,23 @@
+import 'dotenv/config'
 import { CreateCoursesTable1719633870560 } from 'src/migrations/1719633870560-CreateCoursesTable'
-import { dataSourceOptions } from './database.module'
-import { DataSource } from 'typeorm'
+import { DataSource, DataSourceOptions } from 'typeorm'
 import { CreateTagsTable1719634953286 } from 'src/migrations/1719634953286-CreateTagsTable'
 import { CreateCoursesTagsTable1719635779098 } from 'src/migrations/1719635779098-CreateCoursesTagsTable'
 import { AddCoursesIdToCoursesTagsTable1719636346458 } from 'src/migrations/1719636346458-AddCoursesIdToCoursesTagsTable'
 import { AddTagsIdToCoursesTagsTable1719637509464 } from 'src/migrations/1719637509464-AddTagsIdToCoursesTagsTable'
+import { Course } from 'src/courses/entities/courses.entity'
+import { Tag } from 'src/courses/entities/tags.entity'
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [Course, Tag],
+  synchronize: false,
+}
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
